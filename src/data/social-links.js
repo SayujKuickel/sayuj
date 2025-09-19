7ae5fd7ebdaeb7dbaf0131fbcb1cb2773e2d5941
sayujkuickel <sayujkuickel@gmail.com>
2025-09-19 22:05:32 +0545

    !upload

---
+++ src/data/social-links.js
@@ -0,0 +1,17 @@
+export const socialLinks = [
+  {
+    name: "GitHub",
+    url: "https://github.com/SayujKuickel",
+    icon: "fab fa-github",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/sayuj-kuickel-7a1449291",
+    icon: "fab fa-linkedin",
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/sayujkuickel",
+    icon: "fab fa-instagram",
+  },
+];
